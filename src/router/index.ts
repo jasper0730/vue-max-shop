5efc12d794f51fac98414b946be0cbf414c4e5ca
Jasper Li <wilsonlee0730@gmail.com>
2025-02-21 16:03:23 +0800

    update title

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,31 +21,37 @@ const router = createRouter({
           path: '/about',
           name: 'about',
           component: () => import('../views/AboutView.vue'),
+          meta: { title: '關於我們 - Vue Max Shop' },
         },
         {
           path: '/questions',
           name: 'questions',
           component: () => import('../views/QuestionsView.vue'),
+          meta: { title: '常見問題 - Vue Max Shop' },
         },
         {
           path: '/cart',
           name: 'cart',
           component: () => import('../views/CartView.vue'),
+          meta: { title: '購物車 - Vue Max Shop' },
         },
         {
           path: '/products',
           name: 'products',
           component: () => import('../views/ProductsView.vue'),
+          meta: { title: '香氛蠟燭 - Vue Max Shop' },
         },
         {
           path: '/check',
           name: 'checkout',
           component: () => import('../views/CheckView.vue'),
+          meta: { title: '結帳 - Vue Max Shop' },
         },
         {
           path: '/login',
           name: 'login',
           component: () => import('../views/LoginView.vue'),
+          meta: { title: '登入 - Vue Max Shop' },
         },
       ]
     },
@@ -57,18 +63,24 @@ const router = createRouter({
         {
           path: "orders",
           component: () => import("../views/admin/AdminOrders.vue"),
+          meta: { title: '訂單 - Vue Max Shop' },
         },
         {
           path: "products",
           component: () => import("../views/admin/AdminProducts.vue"),
+          meta: { title: '產品 - Vue Max Shop' },
         },
         {
           path: "coupons",
           component: () => import("../views/admin/AdminCoupons.vue"),
+          meta: { title: '優惠券 - Vue Max Shop' },
         },
       ]
     },
   ],
 });
-
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title as string || 'Vue Max Shop';
+  next();
+});
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-max-shop/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
